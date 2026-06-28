180518511aaa63ca33ec6de8fca9b7e1ac534c29
Jocimar B. A. Huss <jocimarspi@gmail.com>
2026-06-27 21:43:59 -0300

    feat(faq): add knockout stage predictions rule

--- src/components/FaqTab.tsx
+++ src/components/FaqTab.tsx
@@ -73,6 +73,7 @@ export default function FaqTab() {
           {renderFaqItem("r3", "faq_r_q3", "faq_r_a3")}
           {renderFaqItem("r4", "faq_r_q4", "faq_r_a4")}
           {renderFaqItem("r5", "faq_r_q5", "faq_r_a5")}
+          {renderFaqItem("r6", "faq_r_q6", "faq_r_a6")}
         </div>
       )}
 
